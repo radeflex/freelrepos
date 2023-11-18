fe28ef0620cc50ad25557e85aee5eb1e8ff6384b
radeflex <nistefio@gmail.com>
2023-11-18 12:35:22 +0300

    add some changes

--- fls-front/src/components/CourseCard/CourseCard.js
+++ fls-front/src/components/CourseCard/CourseCard.js
@@ -1,27 +1,38 @@
 import { useState } from "react";
 import "./CourseCard.css";
-import { Button } from "react-bootstrap";
+import { Button, Accordion, Card, useAccordionButton } from "react-bootstrap";
 
 const CourseCard = ({ head, header, text, dur, img, type }) => {
-  const [value, setValue] = useState(false);
+  function CustomToggle({ children, eventKey }) {
+    const decoratedOnClick = useAccordionButton(eventKey, () =>
+      console.log('totally custom!'),
+    );
+  
+    return (
+      <span
+        style={{ color: "#e8e6e6", cursor: "pointer" }}
+        onClick={decoratedOnClick}
+      >
+        {children}
+      </span>
+    );
+  }
   return (
     <div className="courseCard" style={type === "primary" ? { backgroundColor: "#88b9db"} : type === "main" ? {backgroundColor: "#d45055"} : {}}>
       <div class="c-body">
         <span class="head" style={type === "primary" ? { backgroundColor: "#88b9db", color: "#515659"} : type === "main" ? {color: "#cfcfcf"} : {}}>{head}</span>
         <h3 class="cc-header" style={type === "primary" ? {color: "#313436"} : type === "main" ? {color: "#cfcfcf"} : {}}>{header}</h3>
-        <button
-          className="cc-btn"
-          style={type === "primary" ? { backgroundColor: "#88b9db", color: "#313436"} : type === "main" ? { backgroundColor: "#d45055", color: "#cfcfcf"} : {}}
-          value="false"
-          onClick={(e) => {
-            setValue(!value)
-            console.log(value);
-          }}
-        >
-          Подробнее {">>"}
-        </button>
-        {<p className="c-text" style={type === "primary" ? { color: "#515659"}: type === "main" ? { color: "#515659"}: {}}>{text}</p> && value}
-        <span className="c-dur" style={type === "primary" ? { color: "#515659"} : type === "main" ?{ color: "#cfcfcf"} : {}}>{dur}</span>
+        <Accordion defaultActiveKey="0">
+      <Card style={{border: 0}}>
+        <Card.Header style={type === "primary" ? {padding: 0, border: 0, backgroundColor: "#88b9db"} : type === "main" ? {padding: 0, border: 0, backgroundColor: "#d45055"} : {padding: 0, border: 0, backgroundColor: "rgb(74, 43, 26)" }}>
+          <CustomToggle eventKey="0">{"Подробнее >>"}</CustomToggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="0">
+          <Card.Body style={type === "primary" ? {padding: 0, border: 0, backgroundColor: "#88b9db"} : type === "main" ? {padding: 0, border: 0, backgroundColor: "#d45055"} : {padding: 0, border: 0, backgroundColor: "rgb(74, 43, 26)", color: "#cfcfcf"}}>{text}</Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion>
+        <span className="c-dur" style={type === "primary" ? { color: "#515659"} : type === "main" ? { color: "#cfcfcf"} : {}}>{dur}</span>
         <br />
         <Button className="c-btn">Контакты</Button>
       </div>
